2aef8afdcf9b01a3fd618f3f06f5c12977574236
yym-yumeng123 <18026493873@163.com>
2020-08-25 23:09:33 +0800

    完善 switch 初始样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    Switch1
-  </div>
+<button><span></span></button>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Switch'
+  name: "Switch",
 }
 </script>
 
 <style lang="scss" scoped>
+$outerHeight: 22px;
+$insideHeight: $outerHeight - 4px;
 
+button {
+  position: relative;
+  width: $outerHeight * 2;
+  height: $outerHeight;
+  border: none;
+  border-radius: $outerHeight / 2;
+  background: blue;
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: $insideHeight;
+    height: $insideHeight;
+    border-radius: $insideHeight / 2;
+    background: #fff;
+  }
+}
 </style>
